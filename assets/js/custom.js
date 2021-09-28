b307c32056f3e8adf01267ab157e052117d5d453
llamllap <llamllap@gmail.com>
2021-09-28 12:46:52 +0800

    stlys css

---
+++ assets/js/custom.js
@@ -0,0 +1,60 @@
+﻿
+/*=============================================================
+    Authour URI: #
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDIT US AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+
+            /*====================================
+             EASING PLUGIN SCRIPTS 
+            ======================================*/
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+            /*====================================
+             WOW PLUGIN SCRIPTS 
+            ======================================*/
+            new WOW().init();
+        
+	
+            /*====================================
+            WRITE YOUR SCRIPTS HERE
+            ======================================*/
+
+
+
+
+
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
